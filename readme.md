7413b7bf9a9d4ab4a49056de0c0ca0d0f2d93548
Kurt Villapando <villapando.kurt27@gmail.com>
2026-03-07 12:46:42 +0800

    Inheritance GT March 7

--- readme.md
+++ readme.md
@@ -11,3 +11,7 @@
 ---
 **1.** Description
 ![img_2.png](img_2.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
+![img_5.png](img_5.png)
+![img_6.png](img_6.png)
--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -10,21 +10,6 @@ public class Student extends Person {
 
     }
 
-//    public int getStudentID(){
-//        return studentID;
-//    }
-//
-//    public void setStudentID(int studentID){
-//        this.studentID = studentID;
-//    }
-//
-//    public String getStudentName(){
-//        return studentName;
-//    }
-//
-//    public void setStudentName(String studentName){
-//        this.studentName = studentName;
-//    }
 
     public String getProgram(){
         return program;
